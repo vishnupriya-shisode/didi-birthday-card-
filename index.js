4a49b213cacfa81a9ee28c4087aeb351c8ad0b9c
vishnupriya-shisode <140819184+vishnupriya-shisode@users.noreply.github.com>
2026-02-16 19:17:42 +0000

    initial setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const calendar22El = document.getElementById("calendar22")
+const calendar23El = document.getElementById("calendar23")
+const calendarEl = document.getElementById("calendarScreen")
+const birthdayEl = document.getElementById("birthdayScreen")
+
+calendar22El.addEventListener("click" , function(){
+    calendar22El.style.display = "none"
+    calendar23El.style.display = "flex"
+})
